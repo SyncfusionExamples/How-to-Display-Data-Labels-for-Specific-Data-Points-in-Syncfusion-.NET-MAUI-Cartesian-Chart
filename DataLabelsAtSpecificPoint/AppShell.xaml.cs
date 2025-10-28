873aab207a8f51aead98a557156748ac05dc32ed
DhanarajRajendran <dhanaraj.rajendran@syncfusion.com>
2025-10-28 14:27:20 +0530

    Added the KB sample of displaying the data points based on threshold limit.

---
+++ DataLabelsAtSpecificPoint/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataLabelsAtSpecificPoint
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataLabelsAtSpecificPoint/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+using System.Collections.ObjectModel;
+
+namespace DataLabelsAtSpecificPoint
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly Random _rand = new();
+
+        public ObservableCollection<ChartData> Data { get; set; } = new();
+        public double Threshold { get; set; } = 75;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Generate richer initial dataset (no start/stop streaming)
+            var now = DateTime.Now;
+            // 60 points for the last minute
+            for (int i = 60; i >= 1; i--)
+            {
+                var time = now.AddSeconds(-i).ToString("HH:mm:ss");
+                var value = NextValue(50, 100);
+                var show = value >= Threshold; // label only above threshold
+                Data.Add(new ChartData(time, value, show));
+            }
+
+            BindingContext = this;
+        }
+
+        private double NextValue(double min, double max)
+        {
+            return Math.Round(min + _rand.NextDouble() * (max - min), 2);
+        }
+    }
+}
